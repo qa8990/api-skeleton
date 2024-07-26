219eefe639505672b1c7683d45fde9169e9b7037
QA Services <qa8990.services@gmail.com>
2024-07-26 14:26:51 -0400

    First commit Feat: Skeleton structure

---
+++ app/api/v1/endpoints/example_endpoint.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.db import models, schemas
+from app.db.database import get_db
+
+router = APIRouter()
+
+@router.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.username == user.username).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    fake_hashed_password = user.password + "notreallyhashed"
+    db_user = models.User(username=user.username, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
